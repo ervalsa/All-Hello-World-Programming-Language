305641bd7e9c852f72d65cb8ae1017d156eeb18a
Ervalsa Dwi Nanda <34961795+wival08@users.noreply.github.com>
2020-12-03 14:43:47 +0800

    Update C#

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
+# Created by Wival
